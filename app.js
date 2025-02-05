a92cade0873ec27173d5dec3b07ca2f71176732a
daniele.gattuso <d.gattuso09@gmail.com>
2025-02-05 16:09:38 +0100

    done excercize

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+const postsRouter = require('./routers/posts.js');
 
 app.use(express.static('public'));
+
+
+// definiamo la rotta home
+app.get('/', (req, res) => {
+    res.send("Ciao sono la rotta Home");
+})
+
+// utilizziamo la rotta delle pizze andando a definire la parte iniziale delle rotte
+app.use("/posts", postsRouter)
+
+
+// avvio del server sulla porta specificata
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
